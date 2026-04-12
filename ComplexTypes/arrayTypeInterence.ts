ebc98f02009e056642f7ec19bffca8b0c210912e
Gabriel <gabrieloliveira280807@gmail.com>
2026-04-12 13:19:49 -0300

    feat: Aprofundando mais em tuplas, entendendo como o Typescipt reagea alguns cenários divergentes com tuplas e tipos

---
+++ ComplexTypes/arrayTypeInterence.ts
@@ -0,0 +1,11 @@
+// Since tuples Preview: Docs Loading link description have fixed lengths, we wouldn’t be able to add additional boolean elements to a tuple
+
+
+// Don't change this part:
+let dogTup: [string, string, string, string] = ['dog', 'brown fur', 'curly tail', 'sad eyes'];
+
+// Your code goes here:
+
+let myArr =  dogTup.concat("things", "happy" , "sad", "dog");
+
+myArr[50] = 'not a dog'
